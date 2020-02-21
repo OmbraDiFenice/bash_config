7d8fb2fa4b9989322de4efa54a4914798898c458
Stefano <stefano.stoduto@gmail.com>
2020-02-21 21:52:47 +0100

    vim solarized plugin

--- vim/setup.sh
+++ vim/setup.sh
@@ -2,6 +2,11 @@
 
 cp $INSTALL_DIR/vim/vimrc ~/.vimrc
 
+git clone https://github.com/altercation/vim-colors-solarized.git
+mkdir -p ~/.vim/colors
+mv vim-colors-solarized/colors/solarized.vim ~/.vim/colors/
+rm -rf vim-colors-solarized
+
 if [ ! -f ~/.vim/autoload/plug.vim ]
 then
     curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
